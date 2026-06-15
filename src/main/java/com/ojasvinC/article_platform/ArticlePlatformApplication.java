3524766e5d41acc1bb7746b3dca99c9f3d1151e7
ojasvinb-consultadd <ojasvin.b@consultadd.com>
2026-06-15 23:16:19 +0530

    set up logging using Slf4j

--- src/main/java/com/ojasvinC/article_platform/ArticlePlatformApplication.java
+++ src/main/java/com/ojasvinC/article_platform/ArticlePlatformApplication.java
@@ -2,11 +2,13 @@ package com.ojasvinC.article_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class ArticlePlatformApplication {
-
 	public static void main(String[] args) {
+		log.info("Application Starting");
 		SpringApplication.run(ArticlePlatformApplication.class, args);
 	}
 
